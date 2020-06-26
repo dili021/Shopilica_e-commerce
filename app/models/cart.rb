acea483374722502da2806111c79de479e1ee26c
Stefan Dili <stefan.dili.021@gmail.com>
2020-06-26 13:23:16 +0200

    styled forms and product show

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,6 +11,16 @@ class Cart < ApplicationRecord
     current_product
   end
 
+  def remove_product(product)
+    current_product = line_items.find_by(product_id: product.id)
+    if current_product
+      current_product.decrement(:quantity)
+    else
+      current_product = line_items.new(product_id: product.id)
+    end
+    current_product
+  end
+
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end

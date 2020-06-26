1f6d1e836a90319aa5f74e1c57898944da464b42
Stefan Dili <stefan.dili.021@gmail.com>
2020-06-26 13:44:22 +0200

    added cart item count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def cart_item_count
+    if @cart.line_items.size > 0
+      "<span class=' text-white 
+                    rounded font-bold'>
+        #{item_count}
+       </span>".html_safe
+    end
+  end
+
+  def item_count
+    item_num  = 0
+    @cart.line_items.each do |item|
+      item_num += item.quantity
+    end
+    item_num
+    end
 end
